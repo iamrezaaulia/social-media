e38370b8844593b8fb607143cb62eb4c8317c1f6
iamrezaaulia <iam.rezaaulia@gmail.com>
2020-11-19 21:49:09 +0700

    getPost by Id query

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -2,30 +2,31 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
   type Post {
-    id: ID!,
-    body: String!,
-    username: String!,
+    id: ID!
+    body: String!
+    username: String!
     created: String!
   }
   type User {
-    id: ID!,
-    email: String!,
-    username: String!,
-    token: String!,
+    id: ID!
+    email: String!
+    username: String!
+    token: String!
     created: String!
   }
   input RegisterInput {
-    email: String!,
-    username: String!,
-    password: String!,
+    email: String!
+    username: String!
+    password: String!
     confirmPassword: String!
   }
   input LoginInput {
-    username: String!,
+    username: String!
     password: String!
   }
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
